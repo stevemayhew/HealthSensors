7869eaead5fd33b9abbaa44ddb42fbfa034560b3
Steve Mayhew <mayhew.steve@gmail.com>
2014-07-21 09:05:37 -0700

    Add live data support.
    Live data now from bitwear: Withings_Fetch.py.  Just the main graph now
    with fixed data set (Weight / Fat Ratio).  The fetch script can fetch any
    metric from Withings table.

--- js/draggable-portlet.js
+++ js/draggable-portlet.js
@@ -31,93 +31,82 @@ var DraggablePortlet = function () {
                 e.stopPropagation();
             });
 
-            var data7_1 = [
-                [1354586000000, 185],
-                [1354587000000, 175],
-                [1354588000000, 175],
-                [1354589000000, 170],
-                [1354590000000, 165],
-                [1354591000000, 165],
-                [1354592000000, 165],
-                [1354593000000, 163],
-                [1354594000000, 167],
-                [1354595000000, 167],
-                [1354596000000, 162],
-                [1354597000000, 162]
-            ];
-            var data7_2 = [
-                [1354586000000, 80],
-                [1354587000000, 120],
-                [1354588000000, 120],
-                [1354589000000, 110],
-                [1354590000000, 90],
-                [1354591000000, 85],
-                [1354592000000, 100],
-                [1354593000000, 94],
-                [1354594000000, 94],
-                [1354595000000, 72],
-                [1354596000000, 73],
-                [1354597000000, 80]
-            ];
 
-            $.plot($("#fixed_main_portlet .panel-body"), [{
-                data: data7_1,
-                label: "Weight",
-                lines: {
-                    fill: true
-                }
-            }, {
-                data: data7_2,
-                label: "Heart Rate",
-
-                points: {
-                    show: true
-                },
-                lines: {
-                    show: true
-                },
-                yaxis: 2
-            }
-            ],
+            var withingsDataItems = [
                 {
-                    series: {
-                        lines: {
-                            show: true,
-                            fill: false
-                        },
-                        points: {
-                            show: true,
-                            lineWidth: 2,
-                            fill: true,
-                            fillColor: "#ffffff",
-                            symbol: "circle",
-                            radius: 5
-                        },
-                        shadowSize: 0
-                    },
-                    grid: {
-                        hoverable: true,
-                        clickable: true,
-                        tickColor: "#f9f9f9",
-                        borderWidth: 1,
-                        borderColor: "#eeeeee"
-                    },
-                    colors: ["#79D1CF", "#E67A77"],
-                    tooltip: true,
-                    tooltipOpts: {
-                        defaultTheme: false
-                    },
-                    xaxis: {
-                        mode: "time"
-                    },
-                    yaxes: [{
-                        /* First y axis */
-                    }, {
-                        /* Second y axis */
-                        position: "right" /* left or right */
-                    }]
+                    data_type: 'weight',
+                    data: [],
+                    label: "Weight",
+                    lines: { fill: true },
+                    points: { show: true }
+                },
+                {
+                    data_type: 'fat_ratio',
+                    data: [],
+                    label: "Ratio",
+                    lines: { fill: false },
+                    points: { show: true },
+                    yaxis: 2
                 }
-            );
+            ];
+
+            _.each(withingsDataItems, function(dataItem) {
+                dataItem.ajax_query =  $.get('http://alpha.bitwear.net/Withings/Withings_Fetch.py', {
+                    userName: 'ckoehler',
+                    dataType: dataItem.data_type
+                });
+                dataItem.ajax_query.done(function(data) {
+                    dataItem.data = data;
+                })
+            });
+
+            var queries = _.pluck(withingsDataItems, 'ajax_query');
+
+            $.when.apply(queries).then(function() {
+
+                $.plot($("#fixed_main_portlet .panel-body"), withingsDataItems, {
+                        series: {
+                            lines: {
+                                show: true,
+                                fill: false
+                            },
+                            points: {
+                                show: true,
+                                lineWidth: 2,
+                                fill: true,
+                                fillColor: "#ffffff",
+                                symbol: "circle",
+                                radius: 5
+                            },
+                            shadowSize: 0
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#f9f9f9",
+                            borderWidth: 1,
+                            borderColor: "#eeeeee"
+                        },
+                        colors: ["#79D1CF", "#E67A77"],
+                        tooltip: true,
+                        tooltipOpts: {
+                            defaultTheme: false
+                        },
+                        xaxis: {
+                            mode: "time"
+                        },
+                        yaxes: [
+                            {
+                                /* First y axis */
+                            },
+                            {
+                                /* Second y axis */
+                                position: "right" /* left or right */
+                            }
+                        ]
+                    }
+                );
+            });
 
         }
 
